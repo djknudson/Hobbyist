8bbd9f0248efd50d62cbd552d8b79c2ef069a2f0
github username <github email address>
2020-10-07 13:36:38 -0700

    New Repo

---
+++ app/javascript/packs/index.js
@@ -0,0 +1,8 @@
+let test = "test message"
+console.log(test)
+function killChildren() {
+    const hobbies = document.querySelectorAll(".hobbies")[0]
+    hobbies.innerHTML = ""
+};
+
+killChildren()
